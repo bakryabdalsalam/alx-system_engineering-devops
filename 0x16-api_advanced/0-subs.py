da87d941b86e3a01ea9cd87748f44f1401251160
bakryabdalsalam <bakryabdalsalam6@gmail.com>
2023-08-08 08:25:13 -0700

    fix some error in 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
-    headers = {'User-Agent': '0x16-api_advanced:project:\
-            v1.0.0 (by /u/bakryabdalsalam)'}
+    headers = {
+        'User-Agent': '0x16-api_advanced:project:\
+            v1.0.0 (by /u/Bakryabdalsalam)'
+    }
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -28,3 +30,4 @@ if __name__ == "__main__":
         subreddit = sys.argv[1]
         subscribers = number_of_subscribers(subreddit)
         print("{:d}".format(subscribers))
+
