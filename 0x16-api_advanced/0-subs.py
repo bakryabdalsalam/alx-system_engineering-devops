57d0dc734f4eb396f3273e11029736c6eee43ea9
bakryabdalsalam <bakryabdalsalam6@gmail.com>
2023-08-08 14:27:54 -0700

    update for second time

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 """
-0-subs
+Contains a function that works with Reddit API
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
-    headers = {
-        'User-Agent': '0x16-api_advanced:project:\
-            v1.0.0 (by /u/Bakryabdalsalam)'
-    }
-
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        print("{:d}".format(subscribers))
-
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:\
+            v1.0.0 (by /u/Bakry Abdalsalam)'}
+    res = requests.get(url, headers=headers).json()
+    try:
+        subs = res.get('data').get('subscribers')
+        return subs
+    except Exception:
+        return 0
